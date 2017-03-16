d273062343cdb6044c10c53b3150eb3535d2d15f
Ronald Gualan <rmgs1u16@soton.ac.uk>
2017-03-16 22:34:28 +0000

    Restructure to include R source code

--- local_constant.py
+++
@@ -1,4 +0,0 @@
-# Input and Output folders
-GOOGLE_BOOKS_FOLDER = "data/input/gap-html"
-EXTRACTION_FOLDER = "data/extracted/json-text-only-plus-extras"
-EXTRACTION_COMPILED_FILE = "data/extracted/collection.json"
