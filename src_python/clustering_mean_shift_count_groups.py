0461338c31569ad197490870f52d887f97d19c4e
Ronald Gualan <rmgs1u16@soton.ac.uk>
2017-03-19 16:21:35 +0000

    Count possible groups with mean shift

--- src_python/clustering_mean_shift_count_groups.py
+++ src_python/clustering_mean_shift_count_groups.py
@@ -1,54 +1,56 @@
 import numpy as np
-from sklearn.cluster import MeanShift, estimate_bandwidth
-from sklearn.datasets.samples_generator import make_blobs
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from sklearn.pipeline import Pipeline
 import collection_reader
-
-###############################################################################
-# Load data
-# centers = [[1, 1], [-1, -1], [1, -1]]
-corpus = collection_reader.read_books_corpus()  # Read the documents
-
-pipeline = Pipeline([
-    ('vect', CountVectorizer()),
-    ('tfidf', TfidfTransformer()),
-])
-X = pipeline.fit_transform(corpus)
-X_dense = X.todense()
+import preprocessing_util
+import plot_util
+from sklearn.cluster import MeanShift, estimate_bandwidth
 
 
-###############################################################################
-# Compute clustering with MeanShift
+if __name__ == "__main__":
+    # Read data
+    books = collection_reader.read_books_from_mongo();
+    documents = collection_reader.extract_corpus(books)
 
-# The following bandwidth can be automatically detected using
-bandwidth = estimate_bandwidth(X_dense)
+    print("{} books".format(len(documents)))
+    print([book["book_id3"] for book in books])
 
-ms = MeanShift(bandwidth=bandwidth, bin_seeding=True)
-ms.fit(X_dense)
-labels = ms.labels_
-cluster_centers = ms.cluster_centers_
+    # Create term-document representation
+    X = preprocessing_util.convert_to_term_document(documents, min_df=0.1, max_df=0.9)
 
-labels_unique = np.unique(labels)
-n_clusters_ = len(labels_unique)
+    # SVD
+    X = preprocessing_util.apply_svd(X, min(X.shape))
 
-print("number of estimated clusters : %d" % n_clusters_)
-exit()
+    ###############################################################################
+    # Compute clustering with MeanShift
 
-###############################################################################
-# Plot result
-import matplotlib.pyplot as plt
-from itertools import cycle
+    # The following bandwidth can be automatically detected using
+    bandwidth = estimate_bandwidth(X, quantile=0.1)
 
-plt.figure(1)
-plt.clf()
+    ms = MeanShift(bandwidth=bandwidth, bin_seeding=True)
+    ms.fit(X)
+    labels = ms.labels_
+    cluster_centers = ms.cluster_centers_
 
-colors = cycle('bgrcmykbgrcmykbgrcmykbgrcmyk')
-for k, col in zip(range(n_clusters_), colors):
-    my_members = labels == k
-    cluster_center = cluster_centers[k]
-    plt.plot(X[my_members, 0], X[my_members, 1], col + '.')
-    plt.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
-             markeredgecolor='k', markersize=14)
-plt.title('Estimated number of clusters: %d' % n_clusters_)
-plt.show()
+    labels_unique = np.unique(labels)
+    n_clusters_ = len(labels_unique)
+
+    print("number of estimated clusters : %d" % n_clusters_)
+
+    ###############################################################################
+    # Plot result
+    import matplotlib.pyplot as plt
+    from itertools import cycle
+
+    plt.figure(1)
+    plt.clf()
+
+    colors = cycle('bgrcmykbgrcmykbgrcmykbgrcmyk')
+    for k, col in zip(range(n_clusters_), colors):
+        my_members = labels == k
+        cluster_center = cluster_centers[k]
+        plt.plot(X[my_members, 0], X[my_members, 1], col + '.')
+        plt.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
+                 markeredgecolor='k', markersize=14)
+    plt.title('Estimated number of clusters: %d' % n_clusters_)
+    plt.show()
+
+
