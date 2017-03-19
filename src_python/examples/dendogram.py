047c541ddb27f58af4b6ed286a9482d830abc63b
Ronald Gualan <rmgs1u16@soton.ac.uk>
2017-03-19 23:18:29 +0000

    Minor changes on dendogram generator

--- src_python/examples/dendogram.py
+++ src_python/examples/dendogram.py
@@ -1,39 +1,18 @@
 import collection_reader
 from matplotlib import pyplot as plt
-from scipy.cluster.hierarchy import ward, linkage, dendrogram
+from scipy.cluster.hierarchy import ward, average, complete, dendrogram
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 
-def dendogram1():
-    # Read data
-    books = collection_reader.read_books_from_mongo();
-    documents = collection_reader.extract_corpus(books)
-
-    # Create term-document representation
-    vectorizer = TfidfVectorizer(min_df=0.1, max_df=0.7, use_idf=True)
-    X = vectorizer.fit_transform(documents)
-
-    # ward, single, complete, average, weighted, centroid, median
-    linkage_matrix = linkage(X.todense(), 'ward')
-    dendrogram(
-        linkage_matrix,
-        # truncate_mode="lastp",  # show only the last p merged clusters
-        # p=40,  # show only the last p merged clusters
-        show_leaf_counts=True,  # numbers in brackets are counts, others idx
-        leaf_rotation=60.,
-        leaf_font_size=8,
-        show_contracted=True,  # to get a distribution impression in truncated branches
-    )
-    plt.title('Hierarchical Clustering Dendrogram')
-    plt.show()
-
-
-def dendogram2(labels='titles'):
+def custom_dendogram(label_type='titles', linkage_method='ward'):
     """
     Plots a dendogram
-    :param labels: {'titles', 'ids'}
-    :return:
+    uses cosine similarity
+    :param
+    label_type: {'titles', 'ids'}
+    linkage: {'ward', average}
+    :return: None
     """
 
     # Read data
@@ -41,16 +20,14 @@ def dendogram2(labels='titles'):
     documents = collection_reader.extract_corpus(books)
 
     # Labels
-    titles = []
-    if labels == 'titles':
-        titles = [
+    if label_type == 'titles':
+        labels = [
             "(" + book["book_id3"] + ") " +
             book["title"][:25] + ("..." if len(book["title"]) > 25 else "")
             for book in books]
     else:
-        titles = [
-            "(" + book["book_id3"] + ") " for book in books]
-
+        labels = [
+            "(" + book["book_id3"] + ")" for book in books]
 
     # Create term-document representation
     vectorizer = TfidfVectorizer(min_df=0.1, max_df=0.7, use_idf=True)
@@ -60,10 +37,17 @@ def dendogram2(labels='titles'):
     dist = 1 - cosine_similarity(X)
 
     # Define the linkage_matrix using ward clustering pre-computed distances
-    linkage_matrix = ward(dist)
+    if linkage_method == 'ward':
+        linkage_matrix = ward(dist)
+    elif linkage_method == 'average':
+        linkage_matrix = average(dist)
+    elif linkage_method == 'complete':
+        linkage_matrix = complete(dist)
+    else:
+        raise Exception("Parameter linkage_method is not recognized!")
 
-    fig, ax = plt.subplots(figsize=(15, 20))  # set size
-    ax = dendrogram(linkage_matrix, orientation="right", labels=titles)
+    plt.subplots(figsize=(5, 5))  # set size
+    ax = dendrogram(linkage_matrix, orientation="right", labels=labels)
 
     plt.tick_params(
         axis='x',  # changes apply to the x-axis
@@ -75,9 +59,11 @@ def dendogram2(labels='titles'):
     print(ax["leaves"])
     print(ax["ivl"])
 
-    #plt.tight_layout()  # show plot with tight layout
+    # plt.tight_layout()  # show plot with tight layout
     plt.show()
 
 
 if __name__ == "__main__":
-    dendogram2('ids')
+    custom_dendogram('ids', 'ward')
+    custom_dendogram('ids', 'average')
+    custom_dendogram('ids', 'complete')
